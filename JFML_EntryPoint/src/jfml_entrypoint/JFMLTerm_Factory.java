a75db351f81a5794fad90940f650a1381032195b
Addati <m.addati1@studenti.uniba.it>
2018-08-21 11:05:42 +0200

    Correzione metodi createTsukamotoTermType e createFuzzyTermType della classe JFMLTerm_Factory per poter invocare correttamente i costruttori che ricevono liste di specifici oggetti come parametro.

--- JFML_EntryPoint/src/jfml_entrypoint/JFMLTerm_Factory.java
+++ JFML_EntryPoint/src/jfml_entrypoint/JFMLTerm_Factory.java
@@ -64,18 +64,24 @@ public class JFMLTerm_Factory {
 		return new FuzzyTermType(name,c);
 	}
 	
-	public FuzzyTermType createFuzzyTermType(String name, int type, ArrayList<Double> param)
+	public FuzzyTermType createFuzzyTermType(String name, int type, ArrayList<Object> param)
 	{
-		float[] arFloat = new float[param.size()];
-		for (int i = 0; i < param.size(); i++) {
-			arFloat[i] = param.get(i).floatValue();
+		if(!param.isEmpty() && param.get(0) instanceof Double){
+			float[] arFloat = new float[param.size()];
+			for (int i = 0; i < param.size(); i++) {
+				Double paramDouble = (Double) param.get(i);
+				arFloat[i] = paramDouble.floatValue();
+			}
+			return new FuzzyTermType(name,type,arFloat);
+		}else if(!param.isEmpty() && param.get(0) instanceof PointType){
+			ArrayList<PointType> points = new ArrayList<>();
+			for (int i = 0; i < param.size(); i++) {
+				PointType point = (PointType) param.get(i);
+				points.add(point);
+			}
+			return new FuzzyTermType(name,type,points);
 		}
-		return new FuzzyTermType(name,type,arFloat);
-	}
-	
-	public FuzzyTermType createFuzzyTermType(String name, int type, List<PointType> points)
-	{
-		return new FuzzyTermType(name,type,points);
+		return null;
 	}
 
 	public FuzzyTermType createFuzzyTermType(String name, PointSetShapeType p)
@@ -112,18 +118,24 @@ public class JFMLTerm_Factory {
 		return new TsukamotoTermType();
 	}
 	
-	public TsukamotoTermType createTsukamotoTermType(String name, int type, ArrayList<Double> param)
+	public TsukamotoTermType createTsukamotoTermType(String name, int type, ArrayList<Object> param)
 	{
-		float[] arFloat = new float[param.size()];
-		for (int i = 0; i < param.size(); i++) {
-			arFloat[i] = param.get(i).floatValue();
+		if(!param.isEmpty() && param.get(0) instanceof Double){
+			float[] arFloat = new float[param.size()];
+			for (int i = 0; i < param.size(); i++) {
+				Double paramDouble = (Double) param.get(i);
+				arFloat[i] = paramDouble.floatValue();
+			}
+			return new TsukamotoTermType(name,type,arFloat);
+		}else if(!param.isEmpty() && param.get(0) instanceof PointType){
+			ArrayList<PointType> points = new ArrayList<>();
+			for (int i = 0; i < param.size(); i++) {
+				PointType point = (PointType) param.get(i);
+				points.add(point);
+			}
+			return new TsukamotoTermType(name,type,points);
 		}
-		return new TsukamotoTermType(name,type,arFloat);
-	}
-	
-	public TsukamotoTermType createTsukamotoTermType(String name, int type, List<PointType> param)
-	{
-		return new TsukamotoTermType(name,type,param);
+		return null;
 	}
 	
 	public TsukamotoTermType createTsukamotoTermType(String name, PointSetMonotonicShapeType psm)
