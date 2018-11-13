2506cf449980dccf87b0a1bdbc1cd5e138304f61
nbruno12 <n.bruno12@studenti.uniba.it>
2018-11-13 17:49:10 +0100

    Implementazione file setup.py (configurazione delle dipendenze e dei classpath attraverso il modulo setuptools, secondo gli standard di Python)

---
+++ Py4jfml/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name='py4jfml',
+      version='1.0',
+      description='',
+      url='',
+      author='',
+      author_email='',
+      license='',
+      packages=find_packages(),
+      install_requires=[ 'py4j'],
+      include_package_data=True,
+      zip_safe=False)
+
